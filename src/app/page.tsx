6444d06c1837400a2ee3e299b44cd6ce7905356f
song-chaeyoung <153435389+song-chaeyoung@users.noreply.github.com>
2025-03-03 22:23:32 +0900

    모달 배열 관리 기능 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,24 @@ import { useOverlay } from "@/hooks/useOverlay";
 import React from "react";
 
 const MyModal = ({ close }: { close: () => void }) => {
+  const { open } = useOverlay();
+
   return (
     <div className="bg-white p-6 rounded-lg shadow-lg">
       <h2>모달 창</h2>
       <p>이것은 오버레이 모달입니다.</p>
-      <button onClick={close}>닫기</button>
+      <br />
+      <button className="border p-4 rounded-lg cursor-pointer" onClick={close}>
+        닫기
+      </button>
+      <br />
+      <br />
+      <button
+        className="border p-4 rounded-lg cursor-pointer"
+        onClick={() => open(<MyModal2 close={close} />)}
+      >
+        두번째 모달 열기
+      </button>
     </div>
   );
 };
@@ -17,7 +30,10 @@ const MyModal2 = ({ close }: { close: () => void }) => {
     <div className="bg-white p-6 rounded-lg shadow-lg">
       <h2>모달 창 두번째</h2>
       <p>이것은 오버레이 모달입니다.</p>
-      <button onClick={close}>닫기</button>
+      <br />
+      <button className="border p-4 rounded-lg cursor-pointer" onClick={close}>
+        닫기
+      </button>
     </div>
   );
 };
@@ -26,8 +42,10 @@ const Page = () => {
   const { open, close } = useOverlay();
   return (
     <div className="h-screen flex items-center justify-center flex-col">
-      <button onClick={() => open(<MyModal close={close} />)}>모달 열기</button>
-      <button onClick={() => open(<MyModal2 close={close} />)}>
+      <button
+        className="border p-4 rounded-lg cursor-pointer"
+        onClick={() => open(<MyModal close={close} />)}
+      >
         모달 열기
       </button>
     </div>
